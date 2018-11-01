9d7dfc7453c15d82a306abdf8eece8579a85cb2b
Tim Zaman <tzaman@nvidia.com>
2018-10-31 17:52:59 -0700

    Refactor char data generator into dataset.

--- many_to_one_proto_rnn/main.py
+++ many_to_one_proto_rnn/main.py
@@ -7,78 +7,70 @@ import string
 
 torch.manual_seed(7)
 
-# all_letters = string.ascii_letters + " .,;'"
-max_range = 9
-# all_categories = list(map(str, range(max_range+1)))
-all_categories = list(string.digits)
-n_categories = len(all_categories)
-print('categories:', all_categories)
 
-all_letters = string.digits + string.ascii_lowercase + r" ',:{}'"
+class RangeDataset():
+    max_range = 9
+    all_categories = list(string.digits)
+    n_categories = len(all_categories)
+    all_letters = string.digits
+    n_letters = len(all_letters)
+    def next(self):
+        rmax = np.random.randint(low=0, high=self.max_range+1)
+        rmin = np.random.randint(low=0, high=rmax+1)
+        instance = ''.join(map(str, range(rmin, rmax)))
+        label = np.random.randint(low=0, high=self.max_range+1)
+        instance = str(label) + instance
+        return instance, label
 
-print('letters:', all_letters)
-n_letters = len(all_letters)
+class DictDataset():
+    max_range = 9
+    all_categories = list(string.digits)
+    n_categories = len(all_categories)
+    all_letters = string.digits + string.ascii_lowercase + r" ',:{}'"
+    n_letters = len(all_letters)
+    def next(self):
+        n_samples = np.random.randint(low=0, high=4)
+        d = {}
+        for s in range(n_samples):
+            size = np.random.randint(low=1, high=10)
+            key = np.random.choice(list(string.ascii_lowercase), size=size)
+            key = ''.join(key)
+            d[key] = np.random.randint(low=0, high=self.max_range+1)
+        label1 = np.random.randint(low=0, high=self.max_range+1)
+        label2 = np.random.randint(low=0, high=self.max_range - label1 + 1)
+        label = label1 + label2
+        d['invoker'] = label1
+        d['zeus'] = label2
+        instance = str(d)
+        return instance, label
 
-
-
-
-
-# def get_example():
-#     rmax = np.random.randint(low=0, high=max_range+1)
-#     rmin = np.random.randint(low=0, high=rmax+1)
-#     instance = ''.join(map(str, range(rmin, rmax)))
-#     label = np.random.randint(low=0, high=max_range+1)
-#     instance = str(label) + instance
-#     return instance, label
-
-
-
-def get_example():
-    n_samples = np.random.randint(low=0, high=4)
-    d = {}
-    for s in range(n_samples):
-        size = np.random.randint(low=1, high=10)
-        key = np.random.choice(list(string.ascii_lowercase), size=size)
-        key = ''.join(key)
-        d[key] = np.random.randint(low=0, high=max_range+1)
-    label1 = np.random.randint(low=0, high=max_range+1)
-    label2 = np.random.randint(low=0, high=max_range - label1 + 1)
-    label = label1 + label2
-    d['invoker'] = label1
-    d['zeus'] = label2
-    instance = str(d)
-    return instance, label
-
+dataset = RangeDataset()
+# dataset = DictDataset()
 
 
 # Find letter index from all_letters
 def letterToIndex(letter):
-    return all_letters.find(letter)
+    return dataset.all_letters.find(letter)
 
 # Just for demonstration, turn a letter into a <1 x n_letters> Tensor
 def letterToTensor(letter):
-    tensor = torch.zeros(1, n_letters)
+    tensor = torch.zeros(1, dataset.n_letters)
     tensor[0][letterToIndex(letter)] = 1
     return tensor
 
 # Turn a line into a <line_length x 1 x n_letters>,
 # or an array of one-hot letter vectors
 def lineToTensor(line):
-    tensor = torch.zeros(len(line), 1, n_letters)
+    tensor = torch.zeros(len(line), 1, dataset.n_letters)
     for li, letter in enumerate(line):
         tensor[li][0][letterToIndex(letter)] = 1
     return tensor
-
-# print(letterToTensor('J'))
-
-# print(lineToTensor('Jones').size())
-
 
 
 def categoryFromOutput(output):
     top_n, top_i = output.topk(1)
     category_i = top_i[0].item()
-    return all_categories[category_i], category_i
+    return dataset.all_categories[category_i], category_i
 
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -131,7 +123,7 @@ n_hidden = 128
 model_obj = SimpleRNN
 # model_obj = RNN
 
-model = model_obj(input_size=n_letters, hidden_size=n_hidden, output_size=n_categories)
+model = model_obj(input_size=dataset.n_letters, hidden_size=n_hidden, output_size=dataset.n_categories)
 criterion = nn.NLLLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
 
@@ -149,7 +141,7 @@ def train(line_tensor, category_tensor):
 
 
 for i in range(100000):
-    instance, label = get_example()
+    instance, label = dataset.next()
     o, l = train(line_tensor=lineToTensor(instance), category_tensor=torch.tensor([label]))
     cat, cat_i = categoryFromOutput(o)
     print('loss=%.2f instance=%s label=%s pred=%s' % (l, instance, label, cat))
