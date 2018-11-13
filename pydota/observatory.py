4295b010d988af49c0ac97a32d463ba1407b313e
Tim Zaman <timbobel@gmail.com>
2018-11-12 20:28:04 -0800

    Working observatory (using dedicated server at least).

--- pydota/observatory.py
+++ pydota/observatory.py
@@ -5,7 +5,17 @@ from __future__ import print_function
 
 import asyncore
 import socket
+from struct import unpack
 import time
+import math
+
+import protobuf.CMsgBotWorldState_pb2 as _pb
+import google.protobuf.text_format as txtf
+
+TICKS_PER_SECOND = 30
+
+def dotatime_to_tick(dotatime):
+    return math.floor(dotatime * TICKS_PER_SECOND)
 
 
 class Observatory(asyncore.dispatcher):
@@ -32,24 +42,20 @@ class Observatory(asyncore.dispatcher):
     def handle_expt(self):
         self.close() # connection failed, shutdown
 
-    def recvall(self, buffer_size=4096):
-        buf = self.recv(buffer_size)
-        while buf:
-            yield buf
-            if len(buf) < buffer_size: break
-            buf = self.recv(buffer_size)
-
     def handle_read(self):
         print('handle_read')
 
-        response = b''.join(self.recvall())
-        if not response:
-            # Didn't receive anything. Can happen on bad connections.
-            return
+        n_bytes = unpack("@I", self.recv(4))[0]
+    
+        data = self.recv(n_bytes)
 
-        print('response:', response)
+        parsed_data = _pb.CMsgBotWorldState()
+        parsed_data.ParseFromString(data)
+        dotatime = parsed_data.dota_time
+        tick = dotatime_to_tick(dotatime)
+        print('tick: {}'.format(tick))
 
-        # self.handle_close() # we don't expect more data
+        # self.handle_close()
 
     def handle_close(self):
         self.close()
@@ -61,16 +67,15 @@ PORT_RADIANT = 12120
 PORT_DIRE = 12121
 
 
-
 RETRY_DELAY = 5
 
-while True:
-    try:
-        print('loopin..')
-        radiant_dispatcher = Observatory(host=HOST, port=PORT_RADIANT)
-        dire_dispatcher = Observatory(host=HOST, port=PORT_DIRE)
-        asyncore.loop()
-    except ConnectionRefusedError:
-        del radiant_dispatcher, dire_dispatcher
-        print('Connection refused. Trying again in {} seconds.'.format(RETRY_DELAY))
-        time.sleep(RETRY_DELAY)
+# while True:
+#     try:
+print('loopin..')
+radiant_dispatcher = Observatory(host=HOST, port=PORT_RADIANT)
+# dire_dispatcher = Observatory(host=HOST, port=PORT_DIRE)
+asyncore.loop()
+    # except ConnectionRefusedError:
+    #     del radiant_dispatcher#, dire_dispatcher
+    #     print('Connection refused. Trying again in {} seconds.'.format(RETRY_DELAY))
+    #     time.sleep(RETRY_DELAY)
--- run.sh
+++ run.sh
@@ -7,8 +7,10 @@
 docker run --net=host -it dotabot
 
 /root/Steam/steamapps/common/dota\ 2\ beta/game/dota.sh \
-    -dedicated +map dota +sv_lan 1 +clientport 27006 \
-    -fill_with_bots +sv_hibernate_when_empty 0 +dota_auto_surrender_all_disconnected_timeout 180 +sv_cheats 1 +host_timescale 10
+    -dedicated -insecure +map dota +sv_lan 1 +clientport 27006 +sv_cheats 1 \
+    -fill_with_bots -host_force_frametime_to_equal_tick_interval 1 \
+    -botworldstatetosocket_radiant 12120 -botworldstatetosocket_dire 12121 -botworldstatetosocket_frames 5 -botworldstatesocket_threaded \
+    +sv_hibernate_when_empty 0 +dota_auto_surrender_all_disconnected_timeout 180 +host_timescale 1
 
 
 /root/Steam/steamapps/common/dota\ 2\ beta/game/dota.sh \
@@ -47,3 +49,6 @@ docker run --net=host -it dotabot
 #-botworldstatetosocket_radiant 12120 -botworldstatetosocket_dire 12121 -botworldstatetosocket_frames 10 -botworldstatesocket_threaded
 
 #  -botworldstatetosocket_frames "to only send world state updates every N frames."" 
+
+# Using this on desktop mac (12 nov):
+# -novid -console -condebug -botworldstatetosocket_radiant 12120 -botworldstatetosocket_dire 12121 -botworldstatetosocket_frames 1 -botworldstatesocket_threaded -host_force_frametime_to_equal_tick_interval 1 +sv_cheats 1 +sv_lan 1 +dota_camera_edgemove 0
