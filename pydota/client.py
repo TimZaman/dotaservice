5093b597c7c2855569d4adc21f01a095f95d5305
Tim Zaman <timbobel@gmail.com>
2018-11-23 20:06:55 -0800

    Add DotaService frontend gRPC interface

---
+++ pydota/client.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from grpclib.client import Channel
+
+from protobuf.DotaService_grpc import DotaServiceStub
+from protobuf.DotaService_pb2 import Action
+from protobuf.DotaAction_pb2 import DotaAction
+
+async def main():
+    loop = asyncio.get_event_loop()
+    channel = Channel('127.0.0.1', 50051, loop=loop)
+    stub = DotaServiceStub(channel)
+
+    dota_action = DotaAction(x=1, y=2, z=3)
+    response = await stub.Step(Action(action=dota_action))
+    print('--- Reponse ---\n{}'.format(response))
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
