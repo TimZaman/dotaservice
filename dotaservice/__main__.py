96e4e3b2fb2803f78e7b9be882a4ef3d88b8dea7
Tim Zaman <timbobel@gmail.com>
2018-12-09 15:29:37 -0800

    Add shortcut -p to specify port

--- dotaservice/__main__.py
+++ dotaservice/__main__.py
@@ -30,7 +30,7 @@ if platform not in ["linux", "linux2", "darwin"]:
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--ip", type=str, help="gRPC host ip", default="")
-parser.add_argument("--port", type=int, help="gRPC port", default=13337)
+parser.add_argument("-p", "--port", type=int, help="gRPC port", default=13337)
 parser.add_argument(
     "--game-path",
     type=str,
