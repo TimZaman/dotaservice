1e70eeb159518cd555fe769182ef414760c78a01
Nostrademous <nostrademous@hotmail.com>
2018-12-16 20:22:18 -0500

    gut item buying

---
+++ dotaservice/lua/item_purchase_generic.lua
@@ -0,0 +1,10 @@
+-- Implementing this function in this file
+-- prevents bots from ever buying any items
+-- by default from default bot scripts 
+-- implemented by Valve. For bots to buy
+-- items now, we will have to implement
+-- our own logic for buying items.
+
+function ItemPurchaseThink()
+    return
+end
