f1d11b5385f7ac9380e0d60d72721094aed8895b
Tim Zaman <tzaman@nvidia.com>
2018-12-30 13:59:37 -0800

    Bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dotaservice",
-    version="0.3.0",
+    version="0.3.1",
     author="Tim Zaman",
     author_email="timbobel@gmail.com",
     description="DotaService is a service to play Dota 2 through gRPC",
