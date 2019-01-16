3d2a6218e76e72f3e4419baf97243fd2901f3a42
Nostrademous <nostrademous@hotmail.com>
2019-01-16 17:52:51 -0500

    added ability use functions; added comments

--- dotaservice/lua/actions/use_ability_on_entity.lua
+++ dotaservice/lua/actions/use_ability_on_entity.lua
@@ -2,40 +2,45 @@
 --- AUTHOR: Nostrademous
 -------------------------------------------------------------------------------
 
-local AttackUnit = {}
+local UseAbilityOnEntity = {}
 
-AttackUnit.Name = "Attack Unit"
-AttackUnit.NumArgs = 4
+UseAbilityOnEntity.Name = "Use Ability On Entity"
+UseAbilityOnEntity.NumArgs = 4
 
 -------------------------------------------------
-function AttackUnit:Call( hUnit, hTarget, bOnce, iType )
+function UseAbilityOnEntity:Call( hUnit, intAbilitySlot, hTarget, iType )
     hTarget = hTarget[1]
     if hTarget == -1 then -- Invalid target. Do nothing.
         do return end
     end
     hTarget = GetBotByHandle(hTarget)
 
+    local hAbility = hUnit:GetAbilityInSlot(intAbilitySlot[1])
+    if not hAbility then
+        print('[ERROR]: ', hUnit:GetUnitName(), " failed to find ability in slot ", intAbilitySlot[1])
+        do return end
+    end
+
     iType = iType[1]
  
-    -- Note: we do not test if target can be attacked due 
-    -- to modifiers (e.g., invulnerable), range, or any
-    -- debuffs on the hUnit (e.g., disarmed). We assume
+    -- Note: we do not test if target can be ability-targeted due 
+    -- to modifiers (e.g., spell-immunity), range, mana/cooldowns
+    -- or any debuffs on the hUnit (e.g., silenced). We assume
     -- only valid and legal actions are agent selected
     if not hTarget:IsNull() and hTarget:IsAlive() then
         vLoc = hTarget:GetLocation()
         DebugDrawCircle(vLoc, 25, 255, 0, 0)
         DebugDrawLine(hUnit:GetLocation(), vLoc, 255, 0, 0)
         
-        bOnce = bOnce[1]
         if iType == nil or iType == ABILITY_STANDARD then
-            hUnit:Action_AttackUnit(hTarget, bOnce)
+            hUnit:Action_UseAbilityOnEntity(hAbility, hTarget)
         elseif iType == ABILITY_PUSH then
-            hUnit:ActionPush_AttackUnit(hTarget, bOnce)
+            hUnit:ActionPush_UseAbilityOnEntity(hAbility, hTarget)
         elseif iType == ABILITY_QUEUE then
-            hUnit:ActionQueue_AttackUnit(hTarget, bOnce)
+            hUnit:ActionQueue_UseAbilityOnEntity(hAbility, hTarget)
         end
     end
 end
 -------------------------------------------------
 
-return AttackUnit
+return UseAbilityOnEntity
