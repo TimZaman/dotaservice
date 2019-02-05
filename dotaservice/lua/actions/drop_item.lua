ec234181c1102f8f21e536b2952993d002b5f17a
Nostrademous <nostrademous@hotmail.com>
2019-02-05 14:05:12 -0500

    oops, fixed DropItem bot API call

--- dotaservice/lua/actions/drop_item.lua
+++ dotaservice/lua/actions/drop_item.lua
@@ -23,11 +23,11 @@ function DropItem:Call( hUnit, iSlot, vLoc, iType )
     DebugDrawLine(hUnit:GetLocation(), vLoc, 255, 255, 255)
 
     if iType == nil or iType == ABILITY_STANDARD then
-        hUnit:Action_DropItem(hItem)
+        hUnit:Action_DropItem(hItem, vLoc)
     elseif iType == ABILITY_PUSH then
-        hUnit:ActionPush_DropItem(hItem)
+        hUnit:ActionPush_DropItem(hItem, vLoc)
     elseif iType == ABILITY_QUEUE then
-        hUnit:ActionQueue_DropItem(hItem)
+        hUnit:ActionQueue_DropItem(hItem, vLoc)
     end
 end
 -------------------------------------------------
