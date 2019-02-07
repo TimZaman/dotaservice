9d877c9060b431b8d2ed8993f88a693f3492479c
Nostrademous <nostrademous@hotmail.com>
2019-02-07 09:28:57 -0500

    WIP - hero selection

--- dotaservice/lua/hero_selection.lua
+++ dotaservice/lua/hero_selection.lua
@@ -16,11 +16,7 @@ function GetBotNames ()
     }
 end
 
-local bot_heroes = {"npc_dota_hero_nevermore"}
-
-function Think()
-	-- This gets called (every server tick AND until all heroes are picked).
-	-- This needs to gets called at least once if there is no human.
+local function mode1v1()
     local ids = GetTeamPlayers(GetTeam())
     for i,v in pairs(ids) do
         -- If the human is in the unassigned slot, the radiant bots start at v = 2
@@ -28,13 +24,67 @@ function Think()
         -- If the human is in the first radiant slot, the radiant bots start at v = 0
         -- If the human is in the second radiant slot, the radiant bots start at v = 1
         -- If the human is in the third radiant slot, the radiant bots start at v = 2
-		if IsPlayerBot(v) and IsPlayerInHeroSelectionControl(v) then
+        if IsPlayerBot(v) and IsPlayerInHeroSelectionControl(v) and GetSelectedHeroName(v) == "" then
         -- if i == 1 and GetTeam() == TEAM_RADIANT then
             if i == 1 then
-                SelectHero( v, "npc_dota_hero_nevermore" );
-            else
                 SelectHero( v, "npc_dota_hero_sniper" );
+            else
+                SelectHero( v, "npc_dota_hero_zuus" );
             end
-		end
-	end
+        end
+    end
+end
+
+local function modeAP()
+    local ids = GetTeamPlayers(GetTeam())
+end
+
+local function modeCM()
+    local ids = GetTeamPlayers(GetTeam())
+end
+
+function Think()
+	-- This gets called (every server tick AND until all heroes are picked).
+	-- This needs to gets called at least once if there is no human.
+
+    -- NOTE: other remaining game modes of possible interest
+    -- GAMEMODE_ARDM -- All Random Death Match
+    -- GAMEMODE_ABILITY_DRAFT -- Ability Draft
+
+    local game_mode = GetGameMode()
+    -- All Pick
+    if game_mode == GAMEMODE_AP then
+        modeAP()
+    -- Captain's Mode
+    elseif game_mode == GAMEMODE_CM then
+        modeCM()
+    -- 1v1 Mid
+    elseif game_mode == GAMEMODE_1V1MID then
+        mode1v1()
+    else
+        print("[ERROR] UNKNOWN GAME MODE: ", game_mode)
+        do return end
+    end
+end
+
+-- Function below sets the lane assignments for default bots
+-- Obviously, our own agents will do what they belive best
+function UpdateLaneAssignments()
+    if GetTeam() == TEAM_RADIANT then
+        return {
+            [1] = LANE_MID,
+            [2] = LANE_BOT,
+            [3] = LANE_BOT,
+            [4] = LANE_TOP,
+            [5] = LANE_TOP,
+        }
+    elseif GetTeam() == TEAM_DIRE then
+        return {
+            [1] = LANE_MID,
+            [2] = LANE_BOT,
+            [3] = LANE_BOT,
+            [4] = LANE_TOP,
+            [5] = LANE_TOP,
+        }
+    end
 end
