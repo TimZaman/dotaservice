c4e2d4dd055c90f3aa93b78986ded2f68ef75efc
Tim Zaman <timbobel@gmail.com>
2019-03-09 14:53:52 -0800

    Fix location heights for movement (helps debugging)

--- dotaservice/lua/actions/move_directly.lua
+++ dotaservice/lua/actions/move_directly.lua
@@ -16,15 +16,15 @@ function MoveDirectly:Call( hUnit, vLoc, iType )
         print("[ERROR] - MoveDirectly under death/root/stun")
         do return end
     end
-    
+
     -- print("Moving to: <", vLoc[1],", " vLoc[2], "> from <", hUnit:GetLocation().x, ", ", hUnit:GetLocation().y, ">")
-    
     iType = iType[1]
-    
     vLoc = Vector(vLoc[1], vLoc[2], vLoc[3])
+    -- We replace height automatically from the location.
+    vLoc[3] = GetHeightLevel(vLoc) * 128
     DebugDrawCircle(vLoc, 25, 255, 255 ,255)
     DebugDrawLine(hUnit:GetLocation(), vLoc, 255, 255, 255)
-    
+
     if iType == nil or iType == ABILITY_STANDARD then
         hUnit:Action_MoveDirectly(vLoc)
     elseif iType == ABILITY_PUSH then
